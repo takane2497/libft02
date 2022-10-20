bd3f0863e49e2a138cb9648d88439ebb3a58c327
takane2497 <takane.konagaya@gmail.com>
2022-10-21 01:00:07 +0900

    libft02

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: takonaga <takonaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 23:40:22 by takonaga          #+#    #+#             */
-/*   Updated: 2022/10/20 04:54:28 by takonaga         ###   ########.fr       */
+/*   Updated: 2022/10/21 00:58:52 by takonaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,9 @@ char	*ft_itoa(int n)
 	if (n < 0)
 		tmp = -tmp;
 	count = digits_count(tmp);
-		ret = (char *)ft_calloc(count +2, sizeof(char));
+	if (n < 0)
+		count++;
+	ret = (char *)ft_calloc(count +1, sizeof(char));
 	if (ret == NULL)
 		return (NULL);
 	if (n < 0)
